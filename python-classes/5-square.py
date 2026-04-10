e16fa8216b2ee9b86a0aed3b07bb277160be354b
Aaqsin1337 <saqsin19999999@gmail.com>
2026-04-10 21:50:57 +0000

    Public method my_print to display the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Kvadrat klasını print metodu ilə təyin edir"""
+
+
+class Square:
+    """Kvadratı təmsil edən klas"""
+
+    def __init__(self, size=0):
+        """Kvadratı təyin edir
+        Args:
+            size (int): Kvadratın ölçüsü
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Kvadratın ölçüsünü qaytarır"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Kvadratın ölçüsünü yoxlayır və mənimsədir
+        Args:
+            value (int): Yeni ölçü dəyəri
+        Raises:
+            TypeError: Əgər value integer deyilsə
+            ValueError: Əgər value < 0 olarsa
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Kvadratın sahəsini qaytarır"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Kvadratı # simvolu ilə stdout-da çap edir"""
+        if self.__size == 0:
+            print("")
+            return
+
+        for i in range(self.__size):
+            print("#" * self.__size)
